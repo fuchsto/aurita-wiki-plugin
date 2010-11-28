687add27391ced0431604a534eb2f4d9777e3f19
Tobias Fuchs <twh.fuchs@gmail.com>
2010-11-28 19:49:42 +0100

    Improved display of Media_Asset_Selection_List_Entry

--- lib/modules/gui/media_asset_selection_list_entry.rb
+++ lib/modules/gui/media_asset_selection_list_entry.rb
@@ -11,6 +11,7 @@ module Wiki
       @params         = params
       @media_asset_id = params[:media_asset_id]
       super()
+      add_css_class(:media_asset_selection_list_entry)
     end
 
     def element
@@ -19,7 +20,8 @@ module Wiki
         HTML.div { 
           HTML.img.icon(:src     => '/aurita/images/icons/delete_small.png', 
                         :onclick => "$('#{random_id}').remove();" ) + 
-          @params[:label] 
+          HTML.div.thumb { HTML.img(:src => "/aurita/assets/tiny/asset_#{@media_asset_id}.jpg") } +
+          HTML.div.label { @params[:label] }
         } + 
         HTML.input(:type => :hidden, :name => "#{@params[:name]}", :value => @media_asset_id) 
       }
